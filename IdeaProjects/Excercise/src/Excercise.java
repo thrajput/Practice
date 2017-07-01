0cd4daeb7b0a8f18c6f47604f9b83a427fd021c0
tarun <tarunr211@gmail.com>
2017-07-01 09:04:33 -0400

    Java excercise

---
+++ IdeaProjects/Excercise/src/Excercise.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Tarun on 7/1/2017.
+ */
+
+public class Excercise {
+
+    public int countPrimes(int n)
+    {
+        boolean[] nonPrimes=new boolean[n];
+        int count=0;
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        Excercise ex=new Excercise();
+
+        System.out.println( ex.countPrimes(15));
+
+    }
+
+
+}
